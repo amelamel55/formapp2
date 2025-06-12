f90f1b0dd357f41ef5c698e1cfbf6db9abc7970d
ا <@DESKTOP-98BA9OG>
2025-06-12 19:12:51 +0300

    Add project files.

---
+++ formapp2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace formapp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(27, 12);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(120, 20);
+            this.txtTask.TabIndex = 0;
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.Location = new System.Drawing.Point(27, 49);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(120, 82);
+            this.listBoxTasks.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(36, 150);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(81, 25);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(36, 181);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(81, 25);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "delete";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 212);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "edit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.txtTask);
+            this.Name = "Form1";
+            this.Text = "TO DO LIST";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ formapp2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formapp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                listBoxTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedItem != null)
+            {
+                listBoxTasks.Items.Remove(listBoxTasks.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.");
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedItem != null)
+            {
+                string selectedTask = listBoxTasks.SelectedItem.ToString();
+                txtTask.Text = selectedTask;
+                listBoxTasks.Items.Remove(selectedTask);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedItem != null)
+            {
+                string selectedTask = listBoxTasks.SelectedItem.ToString();
+                txtTask.Text = selectedTask;
+                listBoxTasks.Items.Remove(selectedTask);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
